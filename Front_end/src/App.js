eea48b2ee2765ea580437724fc6a77f14de5c5e8
ejieboy134201 <clisondato_200000000623@uic.edu.ph>
2024-10-01 07:02:44 +0800

    v1.3f testing // running

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import Dashboard1 from './components/pages/dashboard1';
+import Dashboard2 from './components/pages/scan_success';
+import Dashboard3 from './components/pages/dashboard3';
+import MedInfo from './components/pages/medinfo'; // Import your MedInfo component
 
 function App() {
+  const [currentDashboard, setCurrentDashboard] = useState(1);
+
+  const goToDashboard1 = () => setCurrentDashboard(1);
+  const goToDashboard2 = () => setCurrentDashboard(2);
+  const goToDashboard3 = () => setCurrentDashboard(3);
+  const goToMedInfo = () => setCurrentDashboard(4); // New function for MedInfo
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentDashboard === 1 && <Dashboard1 goToDashboard2={goToDashboard2} goToDashboard3={goToDashboard3} />}
+      {currentDashboard === 2 && <Dashboard2 goNext={goToDashboard3} goBack={goToDashboard1} />}
+      {currentDashboard === 3 && <Dashboard3 goBack={goToDashboard1} goNext={goToMedInfo} />} {/* Pass goNext to Dashboard3 */}
+      {currentDashboard === 4 && <MedInfo goBack={goToDashboard3} />} {/* MedInfo Component */}
     </div>
   );
 }

4964d666f32929d88ee9bbe729458292e097e4c9
ejieboy134201 <clisondato_200000000623@uic.edu.ph>
2024-10-01 14:25:20 +0800

    v1.5f testing //runnable

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -9,16 +9,21 @@ function App() {
   const [currentDashboard, setCurrentDashboard] = useState(1);
 
   const goToDashboard1 = () => setCurrentDashboard(1);
-  const goToDashboard2 = () => setCurrentDashboard(2);
-  const goToDashboard3 = () => setCurrentDashboard(3);
-  const goToMedInfo = () => setCurrentDashboard(4); // New function for MedInfo
+  const goToDashboard2 = () => setCurrentDashboard(2); // Function to go to Dashboard2
+  const goToDashboard3 = () => setCurrentDashboard(3); // Function to go to Dashboard3
+  const goToMedInfo = () => setCurrentDashboard(4);    // New function for MedInfo
 
   return (
     <div className="App">
-      {currentDashboard === 1 && <Dashboard1 goToDashboard2={goToDashboard2} goToDashboard3={goToDashboard3} />}
+      {currentDashboard === 1 && (
+        <Dashboard1 
+          goToDashboard2={goToDashboard2}  // Passing goToDashboard2 to Dashboard1
+          goToDashboard3={goToDashboard3}  // Passing goToDashboard3 to Dashboard1
+        />
+      )}
       {currentDashboard === 2 && <Dashboard2 goNext={goToDashboard3} goBack={goToDashboard1} />}
-      {currentDashboard === 3 && <Dashboard3 goBack={goToDashboard1} goNext={goToMedInfo} />} {/* Pass goNext to Dashboard3 */}
-      {currentDashboard === 4 && <MedInfo goBack={goToDashboard3} />} {/* MedInfo Component */}
+      {currentDashboard === 3 && <Dashboard3 goBack={goToDashboard1} goNext={goToMedInfo} />}
+      {currentDashboard === 4 && <MedInfo goBack={goToDashboard3} />}
     </div>
   );
 }
--- Front_end/src/components/pages/medinfo.jsx
+++ Front_end/src/components/pages/medinfo.jsx
@@ -1,28 +1,17 @@
 // src/components/pages/dashboard1.jsx
 import React from 'react';
 import '../css/medinfo.css';
-import logo from '../assets/images/scanseta_logo_white.png'; // Adjust the path as needed
-import scanIcon from '../assets/icons/dashboard1/scan_icon.png'; // Updated image name
-import searchIcon from '../assets/icons/dashboard1/search_icon.png'; // Updated image name
+import back from '../assets/icons/medinfo/back.png'; // Ensure the path is correct
 
-const Dashboard1 = ({ goToDashboard2, goToDashboard3 }) => (
+const Dashboard1 = ({ goToDashboard3 }) => (
   <div className="dashboard">
     <div className="header">
-      <img src={logo} alt="Scanseta Logo" className="logo" />
-    </div>
-    <div className="buttons">
-      <button onClick={goToDashboard2}>
-        <img src={scanIcon} alt="Scan Icon" className="button-image" />
-        <h2>Scan</h2>
-        <p>Scan Doctor’s prescription</p>
-      </button>
-      <button onClick={goToDashboard3}>
-        <img src={searchIcon} alt="Search Icon" className="button-image" />
-        <h2>Medinfo</h2>
-        <p>Discover medicine’s information</p>
+      <button className="back-button" onClick={goToDashboard3}>
+        <img src={back} alt="Back" className="back-icon" />
       </button>
     </div>
   </div>
 );
 
 export default Dashboard1;
+

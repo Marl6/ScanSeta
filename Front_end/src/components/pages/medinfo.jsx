2b1f7b7098b3a0fb3d3061f0296696adee6bc399
ejieboy134201 <clisondato_200000000623@uic.edu.ph>
2024-10-04 13:57:53 +0800

    v1.6f running

--- Front_end/src/components/pages/medinfo.jsx
+++ Front_end/src/components/pages/medinfo.jsx
@@ -9,9 +9,20 @@ const Dashboard1 = ({ goToDashboard3 }) => (
       <button className="back-button" onClick={goToDashboard3}>
         <img src={back} alt="Back" className="back-icon" />
       </button>
+
+      <button className="medicine_name">
+        <h2>Medicine Name</h2>
+      </button>
+    </div>
+
+    {/* New overall-container */}
+    <div className="overall-container">
+      <p>This is your content inside the container.</p>
+      <div className="name-container">
+        <p>This is your content inside the container.</p>
+      </div>
     </div>
   </div>
 );
 
 export default Dashboard1;
-
